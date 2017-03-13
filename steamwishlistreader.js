3528bb5fe925ece6e691309d5d2c07b47510b61b
Maiko Gabriel Kinzel Engelke <ssj4maiko@gmail.com>
2017-03-13 13:20:48 -0300

    Export Done

--- steamwishlistreader.js
+++ steamwishlistreader.js
@@ -85,6 +85,7 @@ function WLR_timeConverter(date){
 }
 
 var  WLR_gamesList = {}
+	,WLR_gamesList_array = []
 	,WLR_content = document.getElementById('wishlist_items')
 	,WLR_form = WLR_content.parentNode
 	,tmp = null
@@ -95,15 +96,15 @@ function WLR_getItem(WLR_row){
 		,logo = findClass(WLR_row,'gameListRowLogo')
 		,item = findClass(WLR_row,'wishlistRowItem');
 
-	contents.id			= WLR_row.id.substr(5);
-	contents.store_url	= logo.children[0].href;
-	contents.icon		= logo.children[0].children[0].src;
-	contents.name		= findTag(item,'h4').textContent;
-	contents.timeAdded	= WLR_timeConverter(findClass(item,'wishlist_added_on').textContent);
+	contents.id			= WLR_row.id.substr(5).trim();
+	contents.store_url	= logo.children[0].href.trim();
+	contents.icon		= logo.children[0].children[0].src.trim();
+	contents.name		= findTag(item,'h4').textContent.trim();
+	contents.timeAdded	= WLR_timeConverter(findClass(item,'wishlist_added_on').textContent).trim();
 	var rank = findClass(item,'wishlist_rank');
 
 	if(rank)
-		contents.rank = rank.value;
+		contents.rank = rank.value.trim();
 	else
 		contents.rank = '';
 
@@ -113,16 +114,92 @@ function WLR_getItem(WLR_row){
 	}
 
 	if(price)
-		contents.price = price.textContent;
+		contents.price = price.textContent.trim();
 	else
 		contents.price = '';
 
 	return contents;
 }
+/**********************
 
-for(var i=0;i<WLR_content.children.length;++i){
-	tmp = WLR_getItem(WLR_content.children[i]);
-	WLR_gamesList[tmp.id] = tmp;
+
+
+
+**********************/
+
+function addButtons(){
+	var  nextTo = document.getElementById('wishlist_sort_options')
+		,base = document.createElement('div')
+		,csvIn = document.createElement('button')
+		,csvOut = document.createElement('button')
+		;
+	csvOut.textContent = "CSV Export";
+	csvIn.textContent = "CSV Import";
+
+	csvOut.addEventListener('click',createCSV);
+
+	base.appendChild(csvIn);
+	base.appendChild(csvOut);
+	base.classList.add('sort_options');
+
+	nextTo.parentNode.insertBefore(base,nextTo);
+
 }
+function WLR_prepare(){
+	if(WLR_gamesList_array.length == 0){
+		for(var i=0;i<WLR_content.children.length;++i){
+			tmp = WLR_getItem(WLR_content.children[i]);
+			WLR_gamesList[tmp.id] = tmp;
+			WLR_gamesList_array.push(tmp);
+		}
+	}
 
-console.log(WLR_gamesList);
+	console.log(WLR_gamesList);
+}
+function download(name, val) {
+	var element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(val));
+	element.setAttribute('download', name);
+
+	element.style.display = 'none';
+	document.body.appendChild(element);
+
+	element.click();
+
+	document.body.removeChild(element);
+}
+function createCSV(e){
+	WLR_prepare();
+	var  separator = ','
+		,textSep = '"'
+		,br = "\n"
+		,text = []
+		,row = []
+		,tmp = []
+		,header = ['Game ID','Original Rank', 'Name','Time Added','Price'];
+
+	for(var i=0;i<header.length;++i){
+		row.push(textSep+header[i]+textSep);
+	}
+	text.push(row.join(separator));
+
+	for(i in WLR_gamesList){
+		tmp = [WLR_gamesList[i].id, WLR_gamesList[i].rank, WLR_gamesList[i].name, WLR_gamesList[i].timeAdded, WLR_gamesList[i].price];
+		row = [];
+		for(var j=0;j<tmp.length;++j){
+			row.push(textSep+tmp[j]+textSep);
+		}
+		text.push(row.join(separator));
+	}
+	download("Wishlist.csv",text.join(br));
+
+	e.preventDefault();
+	return false;
+}
+/**********************
+
+
+
+
+**********************/
+addButtons();
